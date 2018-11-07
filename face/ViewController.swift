5d1795de2a916aecf22ffda996b601b24f2b657a
Candice <gxtgong97@gmail.com>
2018-11-06 22:13:14 -0500

    Initial Commit

---
+++ face/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  face
+//
+//  Created by GongXinting on 11/6/18.
+//  Copyright © 2018 digital memory group. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
